97a865544bd91a82063d54bd1bf51e311122a6e3
pumpomeo <thihoang.31241027692@st.ueh.edu.vn>
2025-11-30 10:05:45 +0700

    trash

--- client/include/rtp/FrameReassembler.hpp
+++ client/include/rtp/FrameReassembler.hpp
@@ -10,35 +10,33 @@
 /**
  * @class FrameReassembler
  * @brief Reassembles fragmented RTP packets into complete frames
- * 
+ *
  * Handles:
  * - Out-of-order packets (sorts by sequence number)
  * - Frame boundary detection (marker bit)
  * - Missing packets (timeout)
  */
-class FrameReassembler
-{
-public:
+class FrameReassembler {
+  public:
     /**
-    * @struct Frame
-    * @brief Complete reassembled frame (JPEG data)
-    */
-    struct Frame
-    {
-        uint32_t timestamp; ///< Timestamp of the frame
-        std::vector<uint8_t> data; ///< JPEG data of the frame
-        uint16_t firstSeq; ///< First sequence number
-        uint16_t lastSeq; ///< Last sequence number
-        int packetCount; ///< Number of packets in the frame
+     * @struct Frame
+     * @brief Complete reassembled frame (JPEG data)
+     */
+    struct Frame {
+        uint32_t timestamp;         ///< Timestamp of the frame
+        std::vector<uint8_t> data;  ///< JPEG data of the frame
+        uint16_t firstSeq;          ///< First sequence number
+        uint16_t lastSeq;           ///< Last sequence number
+        int packetCount;            ///< Number of packets in the frame
     };
 
     using FrameCallback = std::function<void(const Frame& frame)>;
 
-private:
+  private:
     /**
-    * @struct PacketBuffer
-    * @brief Buffer for packets of one frame
-    */
+     * @struct PacketBuffer
+     * @brief Buffer for packets of one frame
+     */
 };
 
 #endif
